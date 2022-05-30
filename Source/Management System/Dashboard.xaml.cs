c25194f4a814a4ac8598ffcb1a85eb25ef9fce37
danganhthi <80770875+danganhthi@users.noreply.github.com>
2022-05-30 23:10:26 +0700

    upcode

---
+++ Source/Management System/Dashboard.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Management_System
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Sự kiện khi ấn các button command
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void btnCommands_Click(object sender, RoutedEventArgs e)
+        {
+            Button curButton = sender as Button;
+            Window mainwindows = Window.GetWindow(curButton);
+            if (curButton.Tag.Equals("btnClose"))
+            {
+                mainwindows.Close();
+            }
+            else if (curButton.Tag.Equals("btnMinim"))
+            {
+                mainwindows.WindowState = WindowState.Minimized;
+            }
+            else if (curButton.Tag.Equals("btnMaxim"))
+            {
+                if (mainwindows.WindowState == WindowState.Maximized)
+                {
+                    mainwindows.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    mainwindows.WindowState = WindowState.Maximized;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sự kiện di chuyển cửa sổ
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ColorZone_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        /// <summary>
+        /// Hiển thị danh sách loại sản phẩm
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Dashboard_Loaded(object sender, RoutedEventArgs e)
+        {
+            _mainFrame.Navigate(new HomePage());
+        }
+    }
+}
